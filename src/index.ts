b1095eda337443cfc8bc290fea3c4ec46ae09128
Arthur Lenoir <lenoir.arthur@gmail.com>
2022-05-14 15:11:02 +0200

    feat: add missing export + remove continuous publishing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { GlobalStyle } from "./GlobalStyle";
+export { default as ThemeProvider } from "./components/ThemeProvider";
 export { LogoNupes, LogoEELV, LogoLFI } from "./components/Logos";
 export { Title } from "./components/Title";
+export { SubTitle } from "./components/SubTitle";
+export { Text } from "./components/Text";
