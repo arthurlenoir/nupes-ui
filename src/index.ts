457a6579d4981d56e0d42dd7eacc4438c48a0bf1
Arthur Lenoir <lenoir.arthur@gmail.com>
2022-05-16 19:00:21 +0200

    feat: add missing exports for Button and Modal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { default as ThemeProvider } from "./components/ThemeProvider";
+export { Button } from "./components/Button";
 export { LogoNupes, LogoEELV, LogoLFI } from "./components/Logos";
+export { Modal } from "./components/Modal";
 export { Title } from "./components/Title";
 export { SubTitle } from "./components/SubTitle";
 export { Text } from "./components/Text";
