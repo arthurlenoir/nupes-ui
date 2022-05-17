b68f7be8b927b5f5485dfb6e60ae818f90f78a8e
maiwann <27999224+maiwann@users.noreply.github.com>
2022-05-17 16:29:28 +0200

    Update Highlight.ts

--- src/components/Highlight.ts
+++ src/components/Highlight.ts
@@ -7,23 +7,5 @@ interface HighlightProps {
 }
 
 export const Highlight = styled.span<HighlightProps>`
-  position: relative;
-  font-family: "Acumin Pro Condensed Black";
-  white-space: nowrap;
-  &:before {
-    content: "";
-    position: absolute;
-    bottom: 0.15em;
-    height: 0.35em;
-    left: -16px;
-    right: -16px;
-    top: auto;
-    transform: scale(1) skew(-8deg);
-    z-index: -1;
-    ${({ theme, variant }) => css`
-      background: ${variant === "secondary"
-        ? theme.colors.secondary.lightBackground
-        : theme.colors.primary.lightBackground};
-    `}
-  }
+  font-weight: bold;
 `;
